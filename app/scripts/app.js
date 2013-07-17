d5c84a2cf296f8f3359b04482ac2e40250e43198
Ben S <bshaw@avalanche.ca>
2013-07-16 17:11:54 -0700

    Ported old components from existing application Have not moved components to new framework yet, they will need to refactored

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,30 @@ angular.module('App', [])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+       .when('/gear', {
+        templateUrl: 'views/gear.html'
+      })
+      .when('/regionList', {
+        templateUrl: 'views/regionList.html'
+      })
+      .when('/regionDetails', {
+        templateUrl: 'views/regionDetails.html'
+      })
+      .when('/danger-scale', {
+        templateUrl: 'views/dangerScale.html'
+      })
+      .when('/details-info', {
+        templateUrl: 'views/detailsInfo.html'
+      })
+      .when('/problems', {
+        templateUrl: 'views/problems.html'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/tou', {
+        templateUrl: 'views/tou.html'
+      })
       .otherwise({
         redirectTo: '/'
       });
