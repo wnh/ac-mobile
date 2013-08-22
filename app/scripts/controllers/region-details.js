14211fc7cfa1f69d19021ce11e132d248f2411cd
Luke <lukenorman@gmail.com>
2013-08-22 13:08:20 -0600

    Changes to ensure 'N/A - No Rating' is displayed whenever there is no rating

--- app/scripts/controllers/region-details.js
+++ app/scripts/controllers/region-details.js
@@ -18,17 +18,33 @@ angular.module('CACMobile')
                          //$scope.data = data;
 
                          //! \todo assumes ordered list and assumes recent  checks against the dates should be made her instead of assuming that bulletin has been published !
-                         $scope.today = data.bulletinResultsOf.BulletinMeasurements.dangerRatings.DangerRating_asArray[0];
-                         $scope.today.day = weekdays[new Date($scope.today.validTime.TimeInstant.timePosition.__text).getDay()]
-                         $scope.tomorrow = data.bulletinResultsOf.BulletinMeasurements.dangerRatings.DangerRating_asArray[1];
-                         $scope.tomorrow.day = weekdays[new Date($scope.tomorrow.validTime.TimeInstant.timePosition.__text).getDay()]
-                         $scope.dayAfter = data.bulletinResultsOf.BulletinMeasurements.dangerRatings.DangerRating_asArray[2];
-                         $scope.dayAfter.day = weekdays[new Date($scope.dayAfter.validTime.TimeInstant.timePosition.__text).getDay()]
+                         var today = data.bulletinResultsOf.BulletinMeasurements.dangerRatings.DangerRating_asArray[0];
+                         var tomorrow = data.bulletinResultsOf.BulletinMeasurements.dangerRatings.DangerRating_asArray[1];
+                         var dayAfter = data.bulletinResultsOf.BulletinMeasurements.dangerRatings.DangerRating_asArray[2];
+
+                         function processDay(day)
+                          {
+                            var alpine = day.dangerRatingAlpValue.__text || "N/A - No Rating";
+                            var alpineClass = (alpine  == "N/A - No Rating") ? "none" : alpine.toLowerCase();
+                            var treeline = day.dangerRatingTlnValue.__text || "N/A - No Rating";
+                            var treelineClass = (treeline  == "N/A - No Rating") ? "none" : treeline.toLowerCase();
+                            var belowTreeline = day.dangerRatingBtlValue.__text || "N/A - No Rating";
+                            var belowTreelineClass = (belowTreeline  == "N/A - No Rating") ? "none" : belowTreeline.toLowerCase();
+                            return {
+                            day: weekdays[new Date(day.validTime.TimeInstant.timePosition.__text).getDay()],
+                            alpine: {text: alpine, css: alpineClass},
+                            treeline: {text: treeline, css: treelineClass},
+                            belowTreeline: {text: belowTreeline, css: belowTreelineClass}
+                            }
+                         };
+                         $scope.today = processDay(today)
+                         $scope.tomorrow = processDay(tomorrow)
+                         $scope.dayAfter = processDay(dayAfter)
                          $scope.confidence = data.bulletinResultsOf.BulletinMeasurements.bulletinConfidence.Components.confidenceLevel;
 
                          $scope.valid = data.validTime.TimePeriod; //! valid.beginPosition.__text | valid.endPosition.__text 
-                         $scope.valid.issued = $scope.valid.beginPosition.__text.replace("T"," ")
-                         $scope.valid.expires = $scope.valid.endPosition.__text.replace("T"," ")
+                         $scope.valid.issued = $scope.valid.beginPosition.__text.replace("T"," ");
+                         $scope.valid.expires = $scope.valid.endPosition.__text.replace("T"," ");
                                          
                     },
                     function(error){
