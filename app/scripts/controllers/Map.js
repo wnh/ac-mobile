2046bb84a03d7df3edf34da538bf85ec8b503e22
Luke <lukenorman@gmail.com>
2013-08-28 14:47:46 -0600

    Full, Size. Maps!

--- app/scripts/controllers/Map.js
+++ app/scripts/controllers/Map.js
@@ -95,6 +95,7 @@ angular.module('CACMobile')
 		 var homeControlDiv = document.createElement('div');
 		 var homeControl = new HomeControl(homeControlDiv, map);
 		 homeControlDiv.index = 1;
+		 homeControlDiv.style.zIndex = '1000';
 		 map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
 		 //!
 
