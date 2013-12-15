e13af03f1c076feadebd936bf25ce792ac9fcbf1
BenShaw <bshaw@avalanche.ca>
2013-12-15 11:25:20 -0800

    Observation Submit Style - added group box for location added lcoaiton name under common header modified submit button colour

--- app/scripts/directives/setLocationMap.js
+++ app/scripts/directives/setLocationMap.js
@@ -7,7 +7,7 @@ angular.module('CACMobile')
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
          var myLatLng = new google.maps.LatLng(scope.location.latitude,scope.location.longitude);
-         var mapOptions = {zoom: 6, streetViewControl: false, zoomControl: true, center: myLatLng, mapTypeId: google.maps.MapTypeId.TERRAIN};
+         var mapOptions = {zoom: 13, streetViewControl: false, zoomControl: true, center: myLatLng, mapTypeId: google.maps.MapTypeId.TERRAIN};
          var map = new google.maps.Map(element[0], mapOptions);
          var marker = new google.maps.Marker({
             position: myLatLng,
