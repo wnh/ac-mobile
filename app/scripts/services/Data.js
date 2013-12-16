83e5ebbbc3221d123cb12c70c35ab7766342cded
BenShaw <bshaw@avalanche.ca>
2013-12-16 14:46:10 -0800

    refactor of get data

--- app/scripts/services/Data.js
+++ app/scripts/services/Data.js
@@ -11,235 +11,97 @@ angular.module('CACMobile')
 
 
 angular.module('CACMobile')
-.factory('Data', function($http,$rootScope,$q, DeviceReady){
+.factory('Data', function($http,$rootScope,$q, $log, platform, DeviceReady){
 
-  //! setup file
-  var fileApi = {available : false, callBacks : []};
+   var getXml = function(url,transform){
+                  var defer = $q.defer();
 
-  window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+                  console.log ("requesting data from " + url);
 
-  function getFileSystem (fileSystem) {
-    
-    //! filesystem is available iterate over our callacks that where registered when the API was not available yet
-    var numItems = fileApi.callBacks.length;
-    fileApi.fileSystem = fileSystem;
-    fileApi.available = true;
-    
-    for (var i = 0; i < numItems; i++) {
-      
-      var func = fileApi.callBacks.pop();
-      
-      if(func != null)
-      {
-        console.log("Action from File API Callback stack performed")
-        func();
+                  $http.get(
+                        url,
+                        {transformResponse:transform}
+                    ).
+                    success(function(data, status) {
+
+                        if (data == null)
+                        {
+                          console.error("Got NULL Data From HTTP");
+                          console.error(status);
+                          defer.reject("NULL Data");
+
+                        }
+                        else
+                        {
+                          console.log("Got Data From HTTP status=", status);
+                          defer.resolve(data);
+                        }
+
+                    }).
+                    error(function(data, status) {
+                        defer.reject(status);
+                    });
+
+
+                  return defer.promise;
+               };
+
+   var transform = function(result) {
+    var json = "";
+
+      try{
+         json = x2js.xml_str2json(result);
       }
-      else
-      {
-        console.error("null function pointer Data File API");
+      catch (e) {
+         console.error("Unable to convert from XML to JSON");
       }
 
-    }
-    //!
-    console.log("getFileSystem");
+    return json;
+  };
 
-  }
-
-  function fail(e) {console.error("Error with filesystem", e);}
-
-    console.log(" Register Device Ready Call back for File API");
-    
-    DeviceReady.addEventListener(
-      function () {
-        //! HACK ! seems that we need to do things differently for phonegap and web maybe make this check actual device ?
-        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) //! \todo generalise this function //(window.webkitStorageInfo){// WEB
-        {      
-          //request quota fails on android / phonegap
-          console.log("Device Ready File API");
-          window.requestFileSystem(LocalFileSystem.PERSISTENT, 0 , getFileSystem, fail);
-        } 
-        else{ // Web
-          // required to make web compliant
-          window.webkitStorageInfo.requestQuota(window.PERSISTENT, 
-          1024*1024*5, 
-          function(grantedBytes) {window.requestFileSystem(window.PERSISTENT, grantedBytes, getFileSystem, fail);}, 
-          fail);     
-        }
-    });
-     
    return {
-     
-     //HTTP Get XML and use transform function to convert to json
-     //{
-       httpGetXml: function(url,transform){
-          var defer = $q.defer();        
-          
-          console.log ("requesting data from " + url);
-            
-          $http.get(
-                url,
-                {transformResponse:transform}
-            ).
-            success(function(data, status) {
 
-                if (data == null)
-                {
-                  console.error("Got NULL Data From HTTP");
-                  console.error(status);
-                  defer.reject("NULL Data");
+      get: function (region, url)
+      {
+          var defer = $q.defer();
 
-                }
-                else
-                {
-                  console.log("Got Data From HTTP status=", status);
-                  defer.resolve(data);
-                }
-                
-            }).
-            error(function(data, status) {
-                defer.reject(status);
-            });
-            
+          var maxTries = 2;
+          var errorCount = 0;
+          var validResponse = false;
 
+          $log.info("Data.get() for region " + region);
+
+          var getData = function ()
+          {
+            getXml(url,transform).then(success, fail);
+          }
+
+          var success = function (data)
+          {
+            defer.resolve(data);
+          }
+
+          var fail = function (error)
+          {
+            errorCount ++;
+            $log.error("Error getting data, errorCount incremented. Error: "+ error);
+
+            if (errorCount < maxTries)
+            {
+              getData();
+            }
+            else
+            {
+              $log.error();
+              defer.reject("Invalid Response To many Retries");
+            }
+          }
+
+          getData();
           return defer.promise;
-       },
-       //! } End httpGetXml
-   
-       //HTTP Get json
-      httpGetJson: function(url){
-        var defer = $q.defer();        
+      }
 
-        console.log ("requesting data from " + url);
-
-        $http.get(url).
-        success(function(data, status) {
-          defer.resolve(data);
-        }).
-        error(function(data, status) {
-          defer.reject(status);
-        });
-
-        return defer.promise;
-      },
-       //! } End httpGetJson
-        
-      //! Read a local file {
-      fileRead: function(fileName){
-      
-         //console.groupCollapsed("File Read");
-         
-        var defer = $q.defer();      
-
-        function failAndUpdate(e) {
-          console.log("Error reading file", e);
-          defer.reject(e);
-          $rootScope.$apply();
-        }
-
-        function gotFileEntry(fileEntry) {
-          console.log("gotFileEntry", fileName);  
-          fileEntry.file(readFile, failAndUpdate);
-        }
-           
-        function readFile(fileEntry) {
-
-          var reader = new FileReader();
-
-          reader.onloadend  = function(e) {
-           //var json = JSON.parse(this.result);
-           defer.resolve(e.target.result);
-           $rootScope.$apply();
-          };
-
-          reader.onError = function(e) {
-           console.log('error'); 
-           defer.reject(e);
-           $rootScope.$apply();
-          };
-
-          try{
-            reader.readAsText(fileEntry);
-          }
-          catch (e){
-            console.error("error reading file", e);
-          }
-
-        }
-
-        function getFile(){ fileApi.fileSystem.root.getFile(fileName, {create: false, exclusive: true}, gotFileEntry, failAndUpdate); }
-         
-        if (fileApi.available == true)
-        {
-          getFile();
-        }
-        else
-        {
-          //fileApi no available register callback so this gets called again when it is available
-          console.log("API Not available callback has been registered file read ");
-          fileApi.callBacks.push(getFile);
-        }
-      
-        
-        return defer.promise;
-       },
-       //! } End fileRead
-       
-       //! Write to a local file {
-       fileWrite: function(fileName, data, type, append){
-         
-         //console.groupCollapsed("FileWrite"); events are asyncronous so we cant group
-         
-         function gotFileEntry(fileEntry) {
-            fileEntry.createWriter(gotFileWriter, fail);
-          }
-         
-          function gotFileWriter(fileWriter) {
-
-            fileWriter.writeStart = function(e){
-              console.log("data write start");
-            }
-
-            fileWriter.onwrite = function(e) {
-              console.log("Data writting to file", fileName);
-            };
-
-            fileWriter.onwriteend = function(e) {
-              console.log("Data written to file", fileName);
-            };
-
-            fileWriter.onerror = function(e) {
-              console.error('Write failed: ' + e.toString());
-            };
-
-            if (append = true){
-              fileWriter.seek(fileWriter.length);
-            }
-
-            function write(data_) {
-              var blob = new Blob(data_, type);
-              fileWriter.write(blob);
-            }
-
-            write(data);
-          }
-
-        function getFile(){ fileApi.fileSystem.root.getFile(fileName, {create: true}, gotFileEntry, fail);};
-         
-        if (fileApi.available == true)
-        {
-          getFile();
-        }
-        else
-        {
-          //fileApi no available register callback so this gets called again when it is available
-          console.log("API Not available callback has been registered file write");
-          fileApi.callBacks.push(getFile);
-        }
-         
-      },
-    //! } End filewrite
-       
    }// End return
-   
+
    }); //End Factory
 
--- app/scripts/services/Forecast.js
+++ app/scripts/services/Forecast.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('CACMobile')
-.factory('Forecast', function($rootScope,$q, Data, RegionDefinition){
-	
+.factory('Forecast', function($rootScope,$q, $log, Data, RegionDefinition){
+
    var weekdays = new Array(7);
                weekdays[0] = "Sunday";
                weekdays[1] = "Monday";
@@ -22,12 +22,12 @@ angular.module('CACMobile')
              {name: 'AspectRange_W' , pos:6},
              {name: 'AspectRange_NW', pos:7}];
 
-   var elevationRange = [{name: 'ElevationLabel_Btl', pos:0}, 
-                         {name: 'ElevationLabel_Tln', pos:1}, 
+   var elevationRange = [{name: 'ElevationLabel_Btl', pos:0},
+                         {name: 'ElevationLabel_Tln', pos:1},
                          {name: 'ElevationLabel_Alp', pos:2}];
 
    function stringCleaner (str){
-      
+
       //! remove HTML tags
       var div = document.createElement('div');
       div.innerHTML = str;
@@ -43,7 +43,7 @@ angular.module('CACMobile')
 
 
    function stringBuilder (source, key, target) {
-         
+
          var result = [];
          for (var a =0; a < target.length; ++a)
          {
@@ -102,14 +102,14 @@ angular.module('CACMobile')
          return result;
          //! \todo assert result.size = problemList.size
       }
-      
+
       function processProblem(problem)
       {
          /*
          advisoryCommentArray = [];
          for (var i = 0; i < problem.travelAdvisoryComment_asArray.size, ++i)
          {
-            advisoryCommentArray[i] = problem.travelAdvisoryComment_asArray[i].__text; 
+            advisoryCommentArray[i] = problem.travelAdvisoryComment_asArray[i].__text;
          }*/
          var minSize = parseInt(problem.expectedAvSize.Values.min.__text) + 2;
          var maxSize = parseInt(problem.expectedAvSize.Values.max.__text) + 1;
@@ -169,7 +169,7 @@ angular.module('CACMobile')
 
       this.avSummary = stringCleaner(data.bulletinResultsOf.BulletinMeasurements.avActivityComment);
       this.snowPackSummary = stringCleaner(data.bulletinResultsOf.BulletinMeasurements.snowpackStructureComment);
-      this.weatherSummary =  stringCleaner(data.bulletinResultsOf.BulletinMeasurements.wxSynopsisComment);   
+      this.weatherSummary =  stringCleaner(data.bulletinResultsOf.BulletinMeasurements.wxSynopsisComment);
 
       this.avyProblems =  ProblemList();
 
@@ -177,7 +177,7 @@ angular.module('CACMobile')
 
          var result = [];
          var problemList = data.bulletinResultsOf.BulletinMeasurements.avProblems.AvProblem_asArray;
-            
+
          if (problemList) {
            if (problemList.length > 0)
            {
@@ -192,10 +192,10 @@ angular.module('CACMobile')
          return result;
          //! \todo assert result.size = problemList.size
       }
-      
+
       function processProblem(problem)
       {
-         
+
          var liklihoodAsInt = {
             "Unlikely" : 1,
             "Possible - Unlikely" : 2,
@@ -205,7 +205,7 @@ angular.module('CACMobile')
             "Very Likely - Likely" : 6,
             "Very Likely" : 7,
             "Certain - Very Likely" : 8,
-            "Certain" : 9 
+            "Certain" : 9
          }
 
          var sizeAsAvalx = {
@@ -236,7 +236,7 @@ angular.module('CACMobile')
             //avProblem.travelAdvisoryComment_asArray
          }
       }
-      
+
       function processDay(dayAlp, dayTln, dayBtl)
       {
          //! \todo assert dayAlp is for today
@@ -258,9 +258,8 @@ angular.module('CACMobile')
       };
    }
 
-   var regionFileName = "regions.json";
    var apply = function () {$rootScope.$apply();};
-   
+
    var transform = function(result) {
 		var json = "";
 
@@ -270,95 +269,72 @@ angular.module('CACMobile')
       catch (e) {
          console.error("Unable to convert from XML to JSON");
       }
-	
+
    	return json;
-	}; 
-		
+	};
+
    return {
 
 	   get: function (region)
 	   {
          var defer = $q.defer();
-         var errorCount  = 0;
-		  
-         //! Get the file for the region from HTTP as xml convert to json
-         function getFromHttp () {
+         var url = RegionDefinition.getUrl(region);
 
-            var url = RegionDefinition.getUrl(region);
-   	  		Data.httpGetXml(url, transform).then(
-				 function (data) // get from http succeeded
-				 {
-					 //! Got Data from HTTP save to file {
-            console.log("received data from http");
-            //console.log(data);
-            var validResponse = false; 
-            var forecast = "";
-            if (data != null && typeof data != 'undefined')
-            {
-              if ( RegionDefinition.get()[region].type === 'cac' )
+         Data.get(region, url).then(
+  				 function (data)
+  				 {
+  					 //! Got Data from HTTP save to file {
+              console.log("Got data");
+
+              var forecast = "";
+              if (data != null && typeof data != 'undefined')
               {
-                if (data.ObsCollection != null && typeof data.ObsCollection != 'undefined') {
-                  forecast = new CacData(data);
-                  validResponse = true;
-                } else {
-                  console.error("Unexpected data format for CacData");
+                if ( RegionDefinition.get()[region].type === 'cac' )
+                {
+                  if (data.ObsCollection != null && typeof data.ObsCollection != 'undefined') {
+                    forecast = new CacData(data);
+                    defer.resolve(forecast);
+                  } else {
+                    defer.reject("Unexpected data format for CacData");
+                    $log.error("Unexpected data format for CacData");
+                  }
                 }
-              }
-              else if ( RegionDefinition.get()[region].type === 'parks' )
-              {
-                if (data.CaamlData != null && typeof data.CaamlData != 'undefined') {
-                  forecast = new ParksData(data);
-                  validResponse = true;
-                } else {
-                  console.error("Unexpected data format for ParksData");
+                else if ( RegionDefinition.get()[region].type === 'parks' )
+                {
+                  if (data.CaamlData != null && typeof data.CaamlData != 'undefined') {
+                    forecast = new ParksData(data);
+                    defer.resolve(forecast);
+                  } else {
+                    defer.reject("Unexpected data format for ParksData");
+                    $log.error("Unexpected data format for ParksData");
+                  }
+                }
+                else
+                {
+                  $log.error('unsupported region');
+                  defer.reject('unsupported region');
                 }
               }
               else
               {
-                console.error('unsupported region');
-              }   
-            }
-
-                              
-            if (validResponse) 
-            {
-              defer.resolve(forecast);
-            } 
-            else 
-            {
-              errorCount ++;
-              console.error("invalid data");
-
-              if (errorCount < 2)
-              {
-                getFromHttp();
+                $log.error("Null or Undefined Data");
+                defer.reject("Null or Undefined Data");
               }
-              else
-              {
-                defer.reject("Invalid Response To many Retries");
-              }
-                
-            }
-                
-         },
 
-				 function (error) // get from http failed
-				 {
-					//! Error Getting Data from HTTP 
-					console.error("error getting xml forecast from http for " + region + "error ", error);
-					defer.reject(error);
-					//! }
-				 });
+           },
 
-         } //! } end function getFromXml  	  
-      
-         getFromHttp();
-           
-         return defer.promise;			 
+  				 function (error) // get data failed
+  				 {
+  					console.error("error getting xml forecast from http for " + region + "error ", error);
+  					defer.reject(error);
+  				 });
+
+
+          return defer.promise;
        }
-      
+
    }
-   
+
 });
 
 
@@ -370,4 +346,4 @@ angular.module('CACMobile')
 
 
 
-   
+
