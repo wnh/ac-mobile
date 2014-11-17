8d7b793dcdc9321161d27032401d27862780ac55
JP Bhavnani <jp.bhavnani@gmail.com>
2014-11-17 16:56:23 -0500

    fixed delay in ability to scroll when expanding problems etc. in forecasts

--- www/js/forecasts-list/forecastsListDetailCtrl.js
+++ www/js/forecasts-list/forecastsListDetailCtrl.js
@@ -1,6 +1,16 @@
 angular.module('acMobile.controllers')
-    .controller('ForecastsListDetailCtrl', function($scope, $timeout, $stateParams, forecast) {
+    .controller('ForecastsListDetailCtrl', function($scope, $timeout, $ionicScrollDelegate, $stateParams, forecast) {
         console.log(forecast);
         $scope.forecast = forecast;
+
+        $scope.resize = function() {
+            //ac-components is built using bootstrap which doesn't have a tap/click handler to elimninate the 300ms
+            //click delay on mobile devices. So we have to let the 300ms delay expire and then resize to ensure the
+            //content of the div is shown onscreen.
+            $timeout(function() {
+                $ionicScrollDelegate.resize();
+            }, 310);
+        };
+
 
     });
