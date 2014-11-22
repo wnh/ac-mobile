0b673494e6bb8f3ab806847f62c76f2dbe16d4ef
JP Bhavnani <jp.bhavnani@gmail.com>
2014-11-22 03:14:39 -0500

    revision to routing/ offline check, now only waits for cordova to be ready if it needs to check network state

--- www/js/app.js
+++ www/js/app.js
@@ -50,24 +50,27 @@ angular.module('acMobile', ['ionic', 'ngCordova', 'auth0', 'angular-storage', 'a
 .run(function($rootScope, acTerms, $state, $cordovaNetwork, $ionicLoading, $ionicPlatform) {
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
 
-        $ionicPlatform.ready()
-            .then(function() {
-                if (toState.name != 'app.terms' && toState != 'app.loading' && !acTerms.termsAccepted()) {
-                    console.log("Terms not accepted - re-routing to terms");
-                    event.preventDefault();
-                    $state.go('app.terms');
-                    //return false;
-                }
-                //console.log(toState);
+        if (toState.name != 'app.terms' && toState != 'app.loading' && !acTerms.termsAccepted()) {
+            console.log("Terms not accepted - re-routing to terms");
+            event.preventDefault();
+            $state.go('app.terms');
+            //return false;
+        }
+        //console.log(toState);
 
-                if (toState.data && toState.data.requiresOnline && $cordovaNetwork.isOffline()) {
-                    $ionicLoading.show({
-                        duration: 5000,
-                        template: "<i class='fa fa-chain-broken'></i> No network connection. This part of the app requires a network connection."
-                    });
-                }
+        if (toState.data && toState.data.requiresOnline) {
+            $ionicPlatform.ready()
+                .then(function() {
+                    if ($cordovaNetwork.isOffline()) {
+                        $ionicLoading.show({
+                            duration: 5000,
+                            template: "<i class='fa fa-chain-broken'></i> No network connection. Some portions of the app will not function without a connection."
+                        });
+                    }
+                });
+        }
 
-            });
+
 
     });
 });
