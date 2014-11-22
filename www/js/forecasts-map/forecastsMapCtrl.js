43f3d96605ad2c05d760dc1dfd63c2ffdde8f5ae
JP Bhavnani <jp.bhavnani@gmail.com>
2014-11-21 20:44:10 -0500

    fixed scrolling when forecast is expanded coming from map view

--- www/js/forecasts-map/forecastsMapCtrl.js
+++ www/js/forecasts-map/forecastsMapCtrl.js
@@ -1,5 +1,5 @@
 angular.module('acMobile.controllers')
-    .controller('ForecastsMapCtrl', function($scope, $timeout, acForecast, resForecasts) {
+    .controller('ForecastsMapCtrl', function($scope, $timeout, acForecast, resForecasts, $ionicScrollDelegate) {
         $scope.current = {
             region: {}
         };
@@ -7,10 +7,16 @@ angular.module('acMobile.controllers')
             visible: false
         };
 
-        //acForecast.fetch().then(function(forecasts) {
         $scope.regions = resForecasts;
-        console.log($scope.regions);
-        //});
+
+        $scope.resize = function() {
+            //ac-components is built using bootstrap which doesn't have a tap/click handler to elimninate the 300ms
+            //click delay on mobile devices. So we have to let the 300ms delay expire and then resize to ensure the
+            //content of the div is shown onscreen.
+            $timeout(function() {
+                $ionicScrollDelegate.resize();
+            }, 310);
+        };
 
         $scope.$watch('current.region', function(newRegion, oldRegion) {
             if (newRegion && newRegion !== oldRegion) {
