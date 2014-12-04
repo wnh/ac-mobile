6f9d55ff15cffb3026b7cb6c64749ad5e532cbf6
JP Bhavnani <jp.bhavnani@gmail.com>
2014-12-04 12:11:41 -0500

    added obs modal + obs modal image viewer

--- www/js/forecasts-map/forecastsMapCtrl.js
+++ www/js/forecasts-map/forecastsMapCtrl.js
@@ -1,5 +1,5 @@
 angular.module('acMobile.controllers')
-    .controller('ForecastsMapCtrl', function($scope, $timeout, acForecast, forecasts, obs, $ionicScrollDelegate) {
+    .controller('ForecastsMapCtrl', function($scope, $timeout, acForecast, forecasts, obs, $ionicModal, $ionicScrollDelegate) {
         angular.extend($scope, {
             current: {},
             drawer: {
@@ -17,6 +17,50 @@ angular.module('acMobile.controllers')
                 $ionicScrollDelegate.resize();
             }, 310);
         };
+
+        function linkHandler(event) {
+            $timeout(function() {
+                $scope.imgSrc = event.currentTarget.href;
+                $scope.showImage = true;
+            }, 0);
+            event.preventDefault();
+            return false;
+        }
+
+        $ionicModal.fromTemplateUrl('templates/ob-modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.obModal = modal;
+        });
+        $scope.showObModal = function() {
+            $scope.obModal.show();
+            $timeout(function() {
+                $("a").on('click', linkHandler);
+            }, 0);
+        };
+        $scope.closeObModal = function() {
+            $("a").off('click', linkHandler);
+            $scope.obModal.hide();
+        };
+
+        function removeTags(text, tag) {
+            var wrapped = $("<div>" + text + "</div>");
+            wrapped.find(tag).remove();
+            return wrapped.html();
+        }
+
+        $scope.showImage = false;
+        $scope.clearImage = function() {
+            $scope.showImage = false;
+            $scope.imgSrc = '';
+        };
+
+        $scope.$on('ac.min.obclicked', function(e, html) {
+            var obHtml = removeTags(html, 'style');
+            $scope.obHtml = obHtml;
+            $scope.showObModal();
+        });
 
         $scope.$watch('current.region', function(newRegion, oldRegion) {
             if (newRegion && newRegion !== oldRegion) {
