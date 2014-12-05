34a9243d4c28927bb97f0762e0eabb41cf250788
JP Bhavnani <jp.bhavnani@gmail.com>
2014-12-05 00:03:12 -0500

    revised acMobileSocialShare service to be fully promised based and eliminated ngCordova's social share implementation

--- www/js/shared/services/acMobileSocialShare.js
+++ www/js/shared/services/acMobileSocialShare.js
@@ -1,36 +1,40 @@
 angular.module('acMobile.services')
-    .service('acMobileSocialShare', function($q, $cordovaSocialSharing) {
+    .service('acMobileSocialShare', function($q) {
 
         this.share = function(provider, link, message, image) {
-
+            var deferred = $q.defer();
             message = message || null;
             image = image || null;
 
             if (provider == "twitter") {
-                return $cordovaSocialSharing
-                    .shareViaTwitter(message, image, link)
-                    .then(onSuccess, onFail);
+                //return $cordovaSocialSharing
+                // .shareViaTwitter(message, image, link)
+                // .then(onSuccess, onFail);
+                window.plugins.socialsharing.shareViaTwitter(message, image, link, onSuccess(deferred), onFail(deferred));
             } else if (provider == "facebook") {
-                return $cordovaSocialSharing
-                    .shareViaFacebook(message + " " + link, image, link)
-                    .then(onSuccess, onFail);
+                // return $cordovaSocialSharing
+                //     .shareViaFacebook(message + " " + link, image, link)
+                // .then(onSuccess, onFail);
+                window.plugins.socialsharing.shareViaFacebook(message, image, link, onSuccess(deferred), onFail(deferred));
             } else if (provider == "googleplus") {
                 //experimental - not enabled yet!
-                window.plugins.socialsharing.shareVia('com.google.android.apps.plus', message, null, null, link, onSuccess, onFail);
+                window.plugins.socialsharing.shareVia('com.google.android.apps.plus', message, null, null, link, onSuccess(deferred), onFail(deferred));
             } else {
-                window.plugins.socialsharing.share(message, null, null, link, onSuccess, onFail);
+                window.plugins.socialsharing.share(message, null, image, link, onSuccess(deferred), onFail(deferred));
             }
+            return deferred.promise;
         };
 
-        function onSuccess(result) {
-            if (result) {
-                console.log(result);
-                return $q.when(result);
-            }
+        function onSuccess(deferred) {
+            return function(result) {
+                deferred.resolve(result);
+            };
         }
 
-        function onFail(error) {
-            console.log(error);
-            return $q.reject(error);
+        function onFail(deferred) {
+            return function(error) {
+                console.log(error);
+                deferred.reject(error);
+            };
         }
     });
