585039a14861becf768e3bdaf56f774fb6337705
JP Bhavnani <jp.bhavnani@gmail.com>
2014-12-18 00:09:44 -0500

    added icons to forecast list

--- www/js/forecasts-list/forecastsListCtrl.js
+++ www/js/forecasts-list/forecastsListCtrl.js
@@ -1,6 +1,6 @@
 angular.module('acMobile.controllers')
-    .controller('ForecastsListCtrl', function($scope, $timeout, acForecast) {
-
+    .controller('ForecastsListCtrl', function($scope, acForecast, AC_API_ROOT_URL) {
+        $scope.rootUrl = AC_API_ROOT_URL;
         acForecast.fetch().then(function(forecasts) {
             $scope.regions = forecasts;
         });
