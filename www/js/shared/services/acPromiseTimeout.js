e11089b5c4e225b66e93a9b62493ce0973f1ca9a
JP Bhavnani <jp.bhavnani@gmail.com>
2015-03-08 13:37:54 -0400

    Created promise wrapper to apply a timeout to any passed promise-returning function

---
+++ www/js/shared/services/acPromiseTimeout.js
@@ -0,0 +1,19 @@
+angular.module('acMobile.services')
+    .factory('acPromiseWrapper', function($q, $timeout) {
+        return function(func, timeout) {
+            var deferred = $q.defer();
+
+            var timer = $timeout(function() {
+                deferred.reject('timeout reached : ' + timeout + 'ms');
+            }, timeout);
+
+            $q.when(func()).then(function(result) {
+                $timeout.cancel(timer);
+                deferred.resolve(result);
+            }, function(error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+    });
