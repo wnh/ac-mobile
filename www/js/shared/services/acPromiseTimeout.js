b54e620261b4b2e799885cf353438dd1ba8cc8b1
JP Bhavnani <jp.bhavnani@gmail.com>
2015-03-20 15:32:07 -0400

    removed debug comments

--- www/js/shared/services/acPromiseTimeout.js
+++ www/js/shared/services/acPromiseTimeout.js
@@ -6,21 +6,21 @@ angular.module('acMobile.services')
 
             self.start = function(func, params, timeout) {
                 var deferred = $q.defer();
-                console.log('starting timer id: ' + self.id);
+                //console.log('starting timer id: ' + self.id);
                 self.timer = $timeout(function() {
-                    console.log(self.id + ': timeout exceeded : ' + timeout + 'ms');
+                    //  console.log(self.id + ': timeout exceeded : ' + timeout + 'ms');
                     deferred.reject('timeout');
                 }, timeout);
 
                 //$q.when(func.apply(this, params))
                 func.apply(this, params)
                     .then(function(result) {
-                        console.log(self.id + ' timer resolved successfully and cancelled');
+                        //console.log(self.id + ' timer resolved successfully and cancelled');
                         $timeout.cancel(self.timer);
                         deferred.resolve(result);
                     }, function(err) {
-                        console.log(self.id + ' error with request function');
-                        console.log(err);
+                        //console.log(self.id + ' error with request function');
+                        //console.log(err);
                         $timeout.cancel(self.timer);
                         deferred.reject(err);
                     });
