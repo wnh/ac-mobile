3317667f906ffaf3be7989359db980ca3da43464
JP Bhavnani <jp.bhavnani@gmail.com>
2015-03-26 16:16:54 -0400

    removed unused debug functions to logout user and purge report data

--- www/js/appCtrl.js
+++ www/js/appCtrl.js
@@ -1,10 +1,4 @@
 angular.module('acMobile.controllers')
     .controller('AppCtrl', function($scope, acUser, acMin) {
-
         $scope.user = acUser;
-        $scope.isDataStored = acMin.draftReports.length || acMin.submittedReports.length;
-
-        $scope.resetStorage = function() {
-            acMin.purgeStoredData();
-        }
     });
