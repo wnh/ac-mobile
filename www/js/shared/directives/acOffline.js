a5d8267ad216adc1b9159ec2ef91a77f0285870c
JP Bhavnani <jp.bhavnani@gmail.com>
2015-03-29 00:18:54 -0400

    Added retry button to reload current state in offline directive

--- www/js/shared/directives/acOffline.js
+++ www/js/shared/directives/acOffline.js
@@ -1,5 +1,5 @@
 angular.module('acMobile.directives')
-    .directive('acOffline', function($cordovaNetwork) {
+    .directive('acOffline', function($cordovaNetwork, $state) {
         return {
             templateUrl: 'js/shared/directives/acOffline.html',
             replace: true,
@@ -7,6 +7,24 @@ angular.module('acMobile.directives')
             scope: {
                 message: '@',
                 isOnline: '='
+            },
+            link: function(scope, element, attrs) {
+                scope.retryOffline = function() {
+                    scope.isOnline = $cordovaNetwork.isOnline();
+                    console.log('state is: ' + scope.isOnline);
+                    if (scope.isOnline) {
+                        if ($state.name == 'app.offline') {
+                            $state.go('app.forecasts-map');
+                        } else {
+                            $state.transitionTo($state.current, $state.current.params, {
+                                reload: true,
+                                inherit: true,
+                                notify: true
+                            });
+                        }
+                    }
+                }
             }
+
         };
     });
